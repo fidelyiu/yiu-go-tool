eccb63ea029de0dd237a28805e041b65beeab215
Fidel_Yiu <fidel-yiu@qq.com>
2021-06-25 14:30:09 +0800

    添加数字线段相交方法

--- int/int_is.go
+++ int/int_is.go
@@ -9,3 +9,40 @@ func IsTrue(num int) bool {
 func IsFalse(num int) bool {
 	return !IsTrue(num)
 }
+
+// IsIntersect 两个线段是否相交
+// - a, b, c, d 四个一维坐标
+// - isContain 端点相等是否属于相交
+//
+// 相交
+// a---------b
+//      c---------d
+func IsIntersect(a, b, c, d int, isContain bool) bool {
+	// 置换最大值最小值
+	if a > b {
+		t := a
+		a = b
+		b = t
+	}
+	if c > d {
+		t := c
+		c = d
+		d = t
+	}
+	// 两个线段包含关系
+	if (a <= c && d <= b) || (c <= a && b <= d) {
+		return true
+	}
+	// 相交关系
+	if isContain {
+		if (a <= c && c <= b) || (a <= d && d <= b) || (c <= a && a <= d) || (c <= b && b <= d) {
+			return true
+		}
+	} else {
+		if (a < c && c < b) || (a < d && d < b) || (c < a && a < d) || (c < b && b < d) {
+			return true
+		}
+	}
+	return false
+
+}
